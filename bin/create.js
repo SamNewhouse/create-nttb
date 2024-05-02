cbed6ffc34254039ff29838b90d8e0f53b9c17c9
Sam Newhouse <60176563+SamNewhouse@users.noreply.github.com>
2024-05-02 10:58:19 +0100

    create-nttb command implemented (#8)

---
+++ bin/create.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+const { execSync } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+
+if (process.argv.length < 3) {
+  console.log("Please provide a name for your application.");
+  console.log("For example: npx create-nttb my-app");
+  process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+const gitRepo = "https://github.com/SamNewhouse/create-nttb";
+
+try {
+  fs.mkdirSync(projectPath);
+} catch (err) {
+  if (err.code === "EEXIST") {
+    console.log(
+      `The file ${projectName} already exists in the current directory. Please give it another name.`
+    );
+  } else {
+    console.log(err);
+  }
+  process.exit(1);
+}
+
+async function main() {
+  try {
+    console.log("Downloading files...");
+    await cloneRepository(gitRepo, projectPath);
+
+    process.chdir(projectPath);
+
+    console.log("Installing dependencies...");
+    execSync("npm install");
+
+    console.log("Removing unnecessary files");
+    if (fs.existsSync("./.git")) {
+      console.log("Cleaning out unnecessary .git directory...");
+      execSync("npx rimraf ./.git");
+    }
+
+    if (fs.existsSync("/bin")) {
+      console.log("Removing unnecessary bin directory...");
+      execSync("npx rimraf ./bin");
+    }
+
+    console.log("Installed successfully. Enjoy!");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+function cloneRepository(repo, destination) {
+  return new Promise((resolve, reject) => {
+    try {
+      execSync(`git clone --depth 1 ${repo} ${destination}`);
+      resolve();
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+main();

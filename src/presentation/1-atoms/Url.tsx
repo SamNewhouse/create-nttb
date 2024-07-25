b525580942bab6a02bd578b49973c21b918049e2
Sam Newhouse <hello@samnewhouse.co.uk>
2024-07-25 20:46:35 +0100

    Added Url Component as example

---
+++ src/presentation/1-atoms/Url.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { FC, HTMLAttributeAnchorTarget, memo, PropsWithChildren, ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+  className?: string;
+  href: string
+  target?: HTMLAttributeAnchorTarget;
+}
+
+const Url: FC<Props> = ({ children, className, href = "#", target }) => {
+  return (
+    <>
+      <Link
+        href={href}
+        className={`text-sm font-medium hover:underline underline-offset-4${className ? " " + className : ""}`}
+        prefetch={false}
+        target={target}
+        rel="noopener"
+      >
+        {children}
+      </Link>
+    </>
+  );
+};
+
+export default memo<PropsWithChildren<Props>>(Url);
--- src/presentation/5-pages/HomePage.tsx
+++ src/presentation/5-pages/HomePage.tsx
@@ -1,6 +1,6 @@
 import { NextPage } from "next";
+import Url from "../1-atoms/Url";
 import BaseLayout from "../4-layouts/BaseLayout";
-import React from "react";
 
 interface Props {
   //
@@ -10,12 +10,23 @@ const HomePage: NextPage<Props> = () => {
   return (
     <>
       <BaseLayout className="home">
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center h-screen">
           <div className="flex flex-col w-100 max-w-md space-y-3">
-            <h1 className="font-serif text-8xl">
+            <h1 className="font-serif text-6xl lg:text-8xl">
               NextJs Typescript Tailwind Boilerplate
             </h1>
-            <h2 className="font-sans text-2xl">by Sam Newhouse</h2>
+            <h2 className="font-sans text-xl lg:text-2xl">by Sam Newhouse</h2>
+            <div className="flex flex-row space-x-2">
+              <Url href="https://www.npmjs.com/package/create-nttb" className="text-md lg:text-lg">
+                <span>nmpjs</span>
+              </Url>
+              <Url href="https://github.com/SamNewhouse/create-nttb" className="text-md lg:text-lg">
+                <span>github</span>
+              </Url>
+            </div>
+          </div>
+          <div>
+            
           </div>
         </div>
       </BaseLayout>

0245f2ffdb3cf6f4018f1a2977cc9788575f82fb
Sam Newhouse <60176563+SamNewhouse@users.noreply.github.com>
2025-01-25 23:27:24 +0000

    3.0.0 :rocket: (#102)
    * 3.0.0 :rocket:
    
    * ErrorPage refactor
    
    * New static 404 page
    
    * add line to 404

--- src/presentation/5-pages/ErrorPage.tsx
+++ src/presentation/5-pages/ErrorPage.tsx
@@ -1,38 +1,19 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import BaseLayout from "../4-layouts/BaseLayout";
-import React from "react";
 
-interface Props {
-  statusCode: number | null;
-}
-
-const ErrorPage: NextPage<Props> = ({ statusCode }) => {
+const ErrorPage: NextPage = () => {
   return (
     <>
       <BaseLayout className="error">
         <div className="flex justify-center items-center h-screen">
           <div className="flex flex-col w-100 max-w-md space-y-3">
-            <h1 className="font-serif text-8xl">
-              {statusCode ? statusCode : "Unknown"}
-            </h1>
-            <h2 className="font-sans text-2xl">
-              {statusCode === 404 ? "Page not found" : "An error occurred"}
-            </h2>
+            <h1 className="font-serif text-8xl">404</h1>
+            <h2 className="font-sans text-2xl">Page not found</h2>
           </div>
         </div>
       </BaseLayout>
     </>
   );
-};
-
-ErrorPage.getInitialProps = async ({ res, err }): Promise<Props> => {
-  const statusCode = !(res && res.statusCode)
-    ? err && err.statusCode
-      ? err.statusCode
-      : null
-    : res.statusCode;
-
-  return { statusCode };
 };
 
 export default ErrorPage;
